070f940e4ccd3c33979b55560c27a07ee8969acc
Thamarai <tmanalan710@gmail.com>
2025-07-25 20:31:22 +0530

    practiced the form response

--- README.md
+++ README.md
@@ -248,3 +248,35 @@ async fn main() -> std::io::Result<()>{
     .await
 }
 ```
+
+# Hour - 4 [Creating a from post request]
+
+``` 
+
+use actix_web::{App,HttpResponse,HttpServer,Responder,web};
+use serde::Deserialize;
+
+#[derive(Deserialize,Debug)]
+struct FormData{
+    name: String,
+    age: u32
+}
+
+async fn handle_form_data(fdata: web::Form<FormData>) -> impl Responder{
+    println!("The data was received {:?}",fdata);
+
+    HttpResponse::Ok().body(format!("Your name and age is {} - {}",fdata.name,fdata.age))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    HttpServer::new(||{
+        App::new()
+        .service(web::resource("/contact").route(web::post().to(handle_form_data)))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
+
+```
--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,26 @@
 
-use actix_web::{App,get,post,HttpResponse,HttpServer,Responder,web};
-use serde::{Serialize,Deserialize};
-use uuid::Uuid; 
+use actix_web::{App,HttpResponse,HttpServer,Responder,web};
+use serde::Deserialize;
 
-#[derive(Deserialize)]
-struct PathInfo{
-    name: String
-}
-
-#[get("/greet/{name}")]
-async fn greetwith_path(info: web::Path<PathInfo>) -> impl Responder{
-    let name = &info.name;
-    HttpResponse::Ok().body(format!("Hello {}",name))
-}
-
-#[derive(Deserialize)]
-struct QueryParams{
-    name: Option<String>,
-    lang: Option<String>
-}
-
-#[get("/query")]
-async fn greetwith_query(info: web::Query<QueryParams>)->impl Responder{
-    let name = info.name.as_deref().unwrap_or("Guest");
-    let lang = info.lang.as_deref().unwrap_or("English");
-
-    HttpResponse::Ok().body(format!("Hello {} your choosen language is {}",name,lang))
-}
-
-// POST request
 #[derive(Deserialize,Debug)]
-struct NewItem{
+struct FormData{
     name: String,
-    description: String,
+    age: u32
 }
 
-#[derive(Serialize,Debug)]
-struct ResultItem{
-    id: String,
-    status: String,
-}
+async fn handle_form_data(fdata: web::Form<FormData>) -> impl Responder{
+    println!("The data was received {:?}",fdata);
 
-#[post("/items")]
-
-async fn create_item(item: web::Json<NewItem>)-> impl Responder{
-    println!("Received new item {:?}",item);
-
-    let item_id = uuid::Uuid::new_v4();
-
-    let response = ResultItem{
-        id: item_id.to_string(),
-        status: "created".to_string(),
-    };
-
-    HttpResponse::Created().json(response)
-
+    HttpResponse::Ok().body(format!("Your name and age is {} - {}",fdata.name,fdata.age))
 }
 
 #[actix_web::main]
-
 async fn main() -> std::io::Result<()>{
-    HttpServer::new(|| {
+    HttpServer::new(||{
         App::new()
-        .service(greetwith_path)
-        .service(greetwith_query)
-        .service(create_item)
+        .service(web::resource("/contact").route(web::post().to(handle_form_data)))
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
 }
-
-
-
